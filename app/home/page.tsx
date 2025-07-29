478410da1cd14068e172e0cd4136229ebe90b2eb
Forzun <vbhavesh219@gmail.com>
2025-07-30 01:09:51 +0530

    RradientMain

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,16 @@
 import Navbar from "@/components/navbar";
 
-export default function Home(){ 
-
-    return <div>
-        <Navbar />
+export default function Home() {
+  return (
+    <div className="container-wrapper relative">
+        <div
+          className="absolute inset-0 -z-10"
+          style={{
+            background:
+              "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(120, 180, 255, 0.25), transparent 70%), #000000",
+          }}
+        />
+      <Navbar />
     </div>
+  );
 }
-
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import AiInput from "@/components/aiInput";
 import { Globe } from "@/components/magicui/globe";
 
 export default function Home() {
-  return <div className="container-wrapper">
-      
+  return <div className="container-wrapper bg-white">
+        <AiInput />
   </div>;
 }
